677b4e1d470aeef574fd85cbf1e71cea2d2e97a1
Soline Foret <soline.foret2022@campus-eni.fr>
2022-04-01 16:17:30 +0200

    ajout CreationProfilServlet + modification UtilisateurDAOImpl

--- src/main/java/fr/eni/enchere/ihm/CreationProfilServlet.java
+++ src/main/java/fr/eni/enchere/ihm/CreationProfilServlet.java
@@ -1,14 +1,18 @@
 package fr.eni.enchere.ihm;
 
 import java.io.IOException;
+import java.time.LocalDate;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import fr.eni.enchere.bll.BLLException;
 import fr.eni.enchere.bll.UtilisateurManager;
 import fr.eni.enchere.bll.UtilisateurManagerSing;
+import fr.eni.enchere.bo.Utilisateur;
 
 /**
  * Servlet implementation class CreationProfilServlet
@@ -17,27 +21,64 @@ import fr.eni.enchere.bll.UtilisateurManagerSing;
 public class CreationProfilServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private UtilisateurManager manager = UtilisateurManagerSing.getInstance();
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public CreationProfilServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		request.getRequestDispatcher("/WEB-INF/creationProfil.jsp").forward(request, response);
+	public CreationProfilServlet() {
+		super();
+		// TODO Auto-generated constructor stub
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		UtilisateurModel model = new UtilisateurModel();
+		request.getRequestDispatcher("/WEB-INF/creationProfil.jsp").forward(request, response);
+
+		if (request.getParameter("BT_ANNULER") != null) {
+			request.getRequestDispatcher("/WEB-INF/page d'accueil jsp").forward(request, response);
+		}
+		
+		if (request.getParameter("BT_CREER") != null) {
+
+			if (request.getParameter("motDePasse").equals(request.getParameter("verifMotDePasse"))) {
+
+				Utilisateur utilisateur = new Utilisateur();
+				utilisateur.setPseudo(request.getParameter("pseudo"));
+				utilisateur.setPrenom(request.getParameter("prenom"));
+				utilisateur.setTelephone(request.getParameter("telephone"));
+				utilisateur.setCodePostal(request.getParameter("codePostal"));
+				utilisateur.setMotDePasse(request.getParameter("motDePasse"));
+				utilisateur.setNom(request.getParameter("nom"));
+				utilisateur.setEmail(request.getParameter("email"));
+				utilisateur.setRue(request.getParameter("rue"));
+				utilisateur.setVille(request.getParameter("ville"));
+
+				try {
+					manager.addUtilisateur(utilisateur);
+					model.setCurrent(utilisateur);
+				} catch (BLLException e) {
+					model.setMessage(e.getMessage());
+				}
+			}
+			
+			request.setAttribute("model", model);
+		}
+		
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
